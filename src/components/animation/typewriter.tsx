2a9fc479726a7738502726e4df030cb92e64d84d
Dipesh <99866329+pilladipesh33@users.noreply.github.com>
2025-01-06 20:36:53 +0530

    build is working animation error fixed

--- src/components/animation/typewriter.tsx
+++ src/components/animation/typewriter.tsx
@@ -11,78 +11,80 @@ const MAIN_FADE_DURATION = 0.25;
 
 const SWAP_DELAY_IN_MS = 5500;
 
-export const TypewriteAnimation = ({ examples }) => {
-  const [exampleIndex, setExampleIndex] = useState(0);
+export const TypewriteAnimation: React.FC<{ examples: string[] }> = ({
+	examples,
+}) => {
+	const [exampleIndex, setExampleIndex] = useState(0);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setExampleIndex((pv) => (pv + 1) % examples.length);
-    }, SWAP_DELAY_IN_MS);
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setExampleIndex((pv) => (pv + 1) % examples.length);
+		}, SWAP_DELAY_IN_MS);
 
-    return () => clearInterval(intervalId);
-  }, []);
+		return () => clearInterval(intervalId);
+	}, []);
 
-  return (
-    <p className="mb-2.5 text-sm font-light uppercase">
-      <span className="inline-block size-2 bg-secondary-light dark:bg-secondary-dark" />
+	return (
+		<p className="mb-2.5 text-sm font-light uppercase">
+			<span className="inline-block size-2 bg-secondary-light dark:bg-secondary-dark" />
 
-      <span className="ml-3 font-cygnito-mono">
-        <span>TITLE:{" "}</span>
-        {examples[exampleIndex].split("").map((l, i) => (
-          <motion.span
-            initial={{
-              opacity: 1,
-            }}
-            animate={{
-              opacity: 0,
-            }}
-            transition={{
-              delay: FADE_DELAY,
+			<span className="ml-3 font-cygnito-mono">
+				<span>TITLE: </span>
+				{examples[exampleIndex].split("").map((l, i) => (
+					<motion.span
+						initial={{
+							opacity: 1,
+						}}
+						animate={{
+							opacity: 0,
+						}}
+						transition={{
+							delay: FADE_DELAY,
 
-              duration: MAIN_FADE_DURATION,
+							duration: MAIN_FADE_DURATION,
 
-              ease: "easeInOut",
-            }}
-            key={`${exampleIndex}-${i}`}
-            className="relative"
-          >
-            <motion.span
-              initial={{
-                opacity: 0,
-              }}
-              animate={{
-                opacity: 1,
-              }}
-              transition={{
-                delay: i * LETTER_DELAY,
+							ease: "easeInOut",
+						}}
+						key={`${exampleIndex}-${i}`}
+						className="relative"
+					>
+						<motion.span
+							initial={{
+								opacity: 0,
+							}}
+							animate={{
+								opacity: 1,
+							}}
+							transition={{
+								delay: i * LETTER_DELAY,
 
-                duration: 0,
-              }}
-            >
-              {l}
-            </motion.span>
+								duration: 0,
+							}}
+						>
+							{l}
+						</motion.span>
 
-            <motion.span
-              initial={{
-                opacity: 0,
-              }}
-              animate={{
-                opacity: [0, 1, 0],
-              }}
-              transition={{
-                delay: i * LETTER_DELAY,
+						<motion.span
+							initial={{
+								opacity: 0,
+							}}
+							animate={{
+								opacity: [0, 1, 0],
+							}}
+							transition={{
+								delay: i * LETTER_DELAY,
 
-                times: [0, 0.1, 1],
+								times: [0, 0.1, 1],
 
-                duration: BOX_FADE_DURATION,
+								duration: BOX_FADE_DURATION,
 
-                ease: "easeInOut",
-              }}
-              className="absolute bottom-[3px] left-[1px] right-0 top-[3px] bg-secondary-light dark:bg-secondary-dark"
-            />
-          </motion.span>
-        ))}
-      </span>
-    </p>
-  );
+								ease: "easeInOut",
+							}}
+							className="absolute bottom-[3px] left-[1px] right-0 top-[3px] bg-secondary-light dark:bg-secondary-dark"
+						/>
+					</motion.span>
+				))}
+			</span>
+		</p>
+	);
 };
